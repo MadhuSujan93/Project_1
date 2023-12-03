b270a6b32529b80a35d205dab7ff6fc5e5fb9c40
MadhuSujan93 <madhusujan593@gmail.com>
2023-12-03 12:29:18 +0530

    New change 2

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -2,6 +2,21 @@
     {{ $listing['title'] }}
 </h1>
 <p>
+    {{ $listing['tags'] }}
+</p>
+<p>
+    {{ $listing['company'] }}
+</p>
+<p>
+    {{ $listing['location'] }}
+</p>
+<p>
+    {{ $listing['email'] }}
+</p>
+<p>
+    {{ $listing['website'] }}
+</p>
+<p>
     {{ $listing['description'] }}
 </p>
-<h1>hello</h1>
+
